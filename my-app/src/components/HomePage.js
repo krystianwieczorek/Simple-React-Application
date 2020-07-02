4f5549ca77a3b7d28acada7b911c33d0c6b7ab3b
Krystian <krystian.wieczorek@onexgroup.pl>
2020-07-02 21:08:13 +0200

    Added Function Component

---
+++ my-app/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron" style={{ textAlign: "center" }}>
+      <h1>Pluralsight Administration</h1>
+      <p>React, Flux and React Router for responsive web-app</p>
+    </div>
+  );
+}
+
+export default HomePage;
