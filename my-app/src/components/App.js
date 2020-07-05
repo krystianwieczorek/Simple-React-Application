c1ce483075c029fa5a7c14c41f40ce6cca4623ad
Krystian <krystian.wieczorek@onexgroup.pl>
2020-07-05 13:49:56 +0200

    Added Redirect

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -4,7 +4,7 @@ import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
 import AboutPage from "./AboutPage";
 import NotFoundPage from "./NotFoundPage";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path='/' exact component={HomePage} />
         <Route path='/courses' component={CoursesPage} />
         <Route path='/about' component={AboutPage} />
+        <Redirect from='/Home' to='/' />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
