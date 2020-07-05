74a55e879f8c1acb86450e576a7b0eaffd502e30
Krystian <krystian.wieczorek@onexgroup.pl>
2020-07-05 19:01:48 +0200

    Added Form and Saving

---
+++ my-app/src/components/CourseForm.js
@@ -0,0 +1,46 @@
+import React from "react";
+import TextImput from './common/TextImput'
+
+function CourseForm(props) {
+    return (
+        <form onSubmit={props.onSubmit}>
+            <TextImput
+                id="title"
+                label="title"
+                onChange={props.onChange}
+                name="title"
+                value={props.course.title}
+            />
+
+            <div className="form-group">
+                <label htmlFor="author">Author</label>
+                <div className="field">
+                    <select
+                        id="author"
+                        name="authorId"
+                        onChange={props.onChange}
+                        value={props.course.authorId || ""}
+                        className="form-control"
+                    >
+                        <option value="" />
+                        <option value="1">Cory House</option>
+                        <option value="2">Scott Allen</option>
+                    </select>
+                </div>
+            </div>
+
+            <TextImput
+                id="category"
+                label="category"
+                name="category"
+                onChange={props.onChange}
+                value={props.course.category}
+            />
+
+
+            <input type="submit" value="Save" className="btn btn-primary" />
+        </form>
+    );
+}
+
+export default CourseForm;
--- my-app/src/components/ManageCoursePage.js
+++ my-app/src/components/ManageCoursePage.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CourseForm from './CourseForm'
+import * as courseApi from '../api/courseApi'
 
 function ManageCoursePage(props) {
+    const [course, setCourse] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: ""
+    })
+
+    function handleChange(event) {
+        const updatedCourse = { ...course, [event.target.name]: event.target.value }
+        setCourse(updatedCourse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        courseApi.saveCourse(course)
+    }
+
     return (
         <>
             <h2>Manage Course</h2>
-            {props.match.params.slug}
+            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} />
         </>
     )
 }
