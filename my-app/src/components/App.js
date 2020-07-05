46bc0dad4897eb3a9bf8742737de35b588678eea
Krystian <krystian.wieczorek@onexgroup.pl>
2020-07-05 12:44:26 +0200

    Added Route and Links

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,20 +1,19 @@
 import React from "react";
 import HomePage from "./HomePage";
-import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import { Route } from "react-router-dom";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/courses") return <CoursesPage />;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage />;
-  }
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Route path='/' exact component={HomePage} />
+      <Route path='/courses' component={CoursesPage} />
+      <Route path='/about' component={CoursesPage} />
+
+
+
     </div>
   );
 }
